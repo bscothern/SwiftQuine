40592f8e8a910c03b449ff15606861c32af73512
Braden Scothern <BradenScothern@gmail.com>
2019-05-02 22:22:02 -0600

    it seems to work!

--- main.swift
+++ main.swift
@@ -1,23 +1,22 @@
 import Foundation
 let q: String = "\""
 let c = ","
-let s = " \\"
+let s = "\\"
 let p: [String] = [
     "import Foundation;let q: String = ", /* quote, slash, quote x 2*/
     ";let c: String = ", /* quote */
     ",", /* quote */
-    ";let s: [String] = [",
+    ";let s = ", /* quote, slash x2, quote */
+    ";let p: [String] = [",
     "]",
-    "var result = p[0]  + q + s + q + q + p[1] + q + p[2] + q + p[3]",
+    "var result = p[0]  + q + s + q + q + p[1] + q + p[2] + q + p[3] + q + s + s + q + p[4]",
     "for l in p { result += q + l + q + c }",
-    "for l in p[4..6] { result += l + ", ";", "}",
-    "result += p[7] + q + p[8] + q + p[9] + p[8] + p[10] + p[8]",
-    "for l in p[11...] { result += l +", "}",
-    "try! result.write(toFile: ", /* quote */ "main.swift", /* quote */ ", atomically: true, encoding: .utf8)",
+    "for l in p[5...7] { result += l + ", ";", "}",
+    "result += p[8] + q + p[9] + q + p[10] + p[9] + p[11] + p[9] + p[12] + q + p[13] + q + p[14]",
+    "try! result.write(toFile: ", /* quote */ "main2.swift", /* quote */ ", atomically: true, encoding: .utf8)",
 ]
-var result = p[0]  + q + s + q + q + p[1] + q + p[2] + q + p[3]
+var result = p[0]  + q + s + q + q + p[1] + q + p[2] + q + p[3] + q + s + s + q + p[4]
 for l in p { result += q + l + q + c }
-for l in p[4...6] { result += l + ";" }
-result += p[7] + q + p[8] + q + p[9] + p[8] + p[10] + p[8]
-for l in p[11...] { result += l + ";" }
+for l in p[5...7] { result += l + ";" }
+result += p[8] + q + p[9] + q + p[10] + p[9] + p[11] + p[9] + p[12] + q + p[13] + q + p[14]
 try! result.write(toFile: "main2.swift", atomically: true, encoding: .utf8)
