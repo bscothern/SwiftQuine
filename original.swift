82621c508da0e97209c3353488be4d0114b140c4
Braden Scothern <BradenScothern@gmail.com>
2019-05-02 22:23:43 -0600

    cleaning up and preventing deletions to preserve formatting

--- original.swift
+++ original.swift
@@ -13,10 +13,10 @@ let p: [String] = [
     "for l in p { result += q + l + q + c }",
     "for l in p[5...7] { result += l + ", ";", "}",
     "result += p[8] + q + p[9] + q + p[10] + p[9] + p[11] + p[9] + p[12] + q + p[13] + q + p[14]",
-    "try! result.write(toFile: ", /* quote */ "main2.swift", /* quote */ ", atomically: true, encoding: .utf8)",
+    "try! result.write(toFile: ", /* quote */ "main.swift", /* quote */ ", atomically: true, encoding: .utf8)",
 ]
 var result = p[0]  + q + s + q + q + p[1] + q + p[2] + q + p[3] + q + s + s + q + p[4]
 for l in p { result += q + l + q + c }
 for l in p[5...7] { result += l + ";" }
 result += p[8] + q + p[9] + q + p[10] + p[9] + p[11] + p[9] + p[12] + q + p[13] + q + p[14]
-try! result.write(toFile: "main2.swift", atomically: true, encoding: .utf8)
+try! result.write(toFile: "main.swift", atomically: true, encoding: .utf8)
