0b34d4d2fde81063100b9549a92c9878e34b1afd
Braden Scothern <BradenScothern@gmail.com>
2019-05-02 22:56:01 -0600

    main.swift now exactly outputs itself to the console

--- main.swift
+++ main.swift
@@ -1,21 +1,28 @@
 let q = "\""
 let c = ","
 let s = "\\"
+let t = "    "
 let p = [
-    "let q: String = ", /* quote, slash, quote x 2*/
-    ";let c: String = ", /* quote */
-    ",", /* quote */
-    ";let s = ", /* quote, slash x2, quote */
-    ";let p = [",
+    "let q = ",
+    "let c = ",
+    ",",
+    "let s = ",
+    "let t = ",
+    "    ",
+    "let p = [",
     "]",
-    "var result = p[0]  + q + s + q + q + p[1] + q + p[2] + q + p[3] + q + s + s + q + p[4]",
-    "for l in p { result += q + l + q + c }",
-    "for l in p[5...7] { result += l + ", ";", "}",
-    "result += p[8] + q + p[9] + q + p[10] + p[9] + p[11] + p[9] + p[12]",
-    "print(result)"
+    "print(p[0]  + q + s + q + q)",
+    "print(p[1] + q + p[2] + q)",
+    "print(p[3] + q + s + s + q)",
+    "print(p[4] + q + p[5] + q)",
+    "print(p[6])",
+    "for l in p { print(t + q + l + q + c) }",
+    "for l in p[7...] { print(l) }",
 ]
-var result = p[0]  + q + s + q + q + p[1] + q + p[2] + q + p[3] + q + s + s + q + p[4]
-for l in p { result += q + l + q + c }
-for l in p[5...7] { result += l + ";" }
-result += p[8] + q + p[9] + q + p[10] + p[9] + p[11] + p[9] + p[12]
-print(result)
+print(p[0]  + q + s + q + q)
+print(p[1] + q + p[2] + q)
+print(p[3] + q + s + s + q)
+print(p[4] + q + p[5] + q)
+print(p[6])
+for l in p { print(t + q + l + q + c) }
+for l in p[7...] { print(l) }
