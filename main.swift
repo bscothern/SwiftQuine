c1e338f3baa1a3209ea9fc001a74a6ccad709252
Braden Scothern <BradenScothern@gmail.com>
2019-05-02 22:36:46 -0600

    Executing main.swift now prints to the console an executable that produces itself

--- main.swift
+++ main.swift
@@ -12,7 +12,7 @@ let p = [
     "var result = p[0]  + q + s + q + q + p[1] + q + p[2] + q + p[3] + q + s + s + q + p[4]",
     "for l in p { result += q + l + q + c }",
     "for l in p[5...7] { result += l + ", ";", "}",
-    "result += p[8] + q + p[9] + q + p[10] + p[9] + p[11] + p[9] + p[12] + q + p[13] + q + p[14]",
+    "result += p[8] + q + p[9] + q + p[10] + p[9] + p[11] + p[9] + p[12]",
     "print(result)"
 ]
 var result = p[0]  + q + s + q + q + p[1] + q + p[2] + q + p[3] + q + s + s + q + p[4]
